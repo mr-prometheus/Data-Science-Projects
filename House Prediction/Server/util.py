2da5664fe1f0c6c2e1e763571395afeb1257a5d6
mr-prometheus <deepuadak@gmail.com>
2022-07-31 22:43:11 +0530

    Update util.py

--- House Prediction/Server/util.py
+++ House Prediction/Server/util.py
@@ -1,3 +1,24 @@
 import numpy as np
 import json
 import pickle
+
+__locations = None
+__data_columns = None
+__model = None
+
+def get_location_names():
+    return __locations
+
+def load_saved_artificats():
+    print("loading saved artifacts.....start")
+    global __data_columns
+    global __locations
+    with open("./artifact/columns.json","r") as f:
+        __data_columns = json.load(f)['data_columns']
+        __locations = __data_columns[3:]
+    
+    global __model
+    if __model is None:
+        with open('./artifacts/banglore_home_prices_model.pickle', 'rb') as f:
+            __model = pickle.load(f)
+    print("loading saved artifacts...done")
