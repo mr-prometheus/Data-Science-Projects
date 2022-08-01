2f8f45b9f7adaa210a9709d9b1810119f689b8dd
mr-prometheus <deepuadak@gmail.com>
2022-08-01 15:25:41 +0530

    Util File Completed

--- House Prediction/Server/util.py
+++ House Prediction/Server/util.py
@@ -9,20 +9,39 @@ __model = None
 def get_location_names():
     return __locations
 
+def get_data_columns():
+    return __data_columns
+
+def get_estimated_price(location,sqft,bhk,bath):
+    try:
+        loc_index = __data_columns.index[location.lower()]
+    except:
+        loc_index = 1
+    
+    x = np.zeros(len(__data_columns))
+    x[0] = sqft
+    x[1] = bath
+    x[2] = bhk
+    
+    if loc_index>=0:
+        x[loc_index] = 1
+    
+    return round(__model.predict([x])[0],2)
 def load_saved_artificats():
     print("loading saved artifacts.....start")
     global __data_columns
     global __locations
-    with open("./artifacts/columns.json", "r") as f:
+    with open("./Server/artifacts/columns.json", "r") as f:
         __data_columns = json.load(f)['data_columns']
         __locations = __data_columns[3:]  # first 3 columns are sqft, bath, bhk
 
     global __model
     if __model is None:
-        with open('./artifacts/banglore_home_prices_model.pickle', 'rb') as f:
+        with open('./Server/artifacts/banglore_home_prices_model.pickle', 'rb') as f:
             __model = pickle.load(f)
     print("loading saved artifacts...done")
     
 if __name__ == "__main__":
     load_saved_artificats()
-    print(get_location_names())
+    #print(get_location_names())
+    print(get_estimated_price('1st Phase JP Nagar', 1000, 2, 2))
